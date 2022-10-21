3164c968e620773605bb25a6f1a07c4680145dd0
Rafael Neris <rafael.cruz@picpay.com>
2022-10-21 00:34:29 -0300

    BucketTest fix

--- tests/Case3/BucketTest.php
+++ tests/Case3/BucketTest.php
@@ -9,13 +9,10 @@ use Rafaelneris\ObjectCalisthenics\Case3\ProductCollection;
 
 class BucketTest extends TestCase
 {
-    public function testAddAndGetProduct(): void
+    public function testAddProductAndGetProduct(): void
     {
-        $productCollection = (new ProductCollection())->add(new Product());
-
-        $bucket = new Bucket($productCollection);
+        $bucket = new Bucket([new Product()]);
 
         self::assertEquals( 1, $bucket->getCountOfProducts());
     }
-
 }
