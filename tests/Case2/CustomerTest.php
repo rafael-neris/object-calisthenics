1a9933bc0f4ca04d505dd5f63a088a380b77cfe1
Rafael Neris <rafael.cruz@picpay.com>
2022-10-21 00:37:52 -0300

    BucketTest fix

--- tests/Case2/CustomerTest.php
+++ tests/Case2/CustomerTest.php
@@ -3,28 +3,29 @@
 namespace Case2;
 
 use PHPUnit\Framework\TestCase;
+use Rafaelneris\ObjectCalisthenics\Case2\Birthday;
 use Rafaelneris\ObjectCalisthenics\Case2\Customer;
+use Rafaelneris\ObjectCalisthenics\Case2\InvalidBirthdayException;
 
 class CustomerTest extends TestCase
 {
     public function testInstantiateCustomer()
     {
-        $customer = new Customer('John Doe', '1983-02-10');
+        $customer = new Customer('John Doe', new Birthday('1983-02-10'));
 
         self::assertEquals('1983-02-10', $customer->getBirthDay());
     }
 
     public function testInstantiateCustomerWithBrazilianDate()
     {
-        $customer = new Customer('John Doe', '10/02/1983');
+        $customer = new Customer('John Doe', new Birthday('10/02/1983'));
 
         self::assertEquals('1983-02-10', $customer->getBirthDay());
     }
 
     public function testInstantiateCustomerWithOtherDate()
     {
-        $customer = new Customer('John Doe', '10/31/1983');
-
-        self::assertEquals('1983-10-31', $customer->getBirthDay());
+        self::expectException(InvalidBirthdayException::class);
+         new Customer('John Doe', new Birthday('10/]2331/1983'));
     }
 }
